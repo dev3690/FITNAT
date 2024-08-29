5e8caf385a8a76a8ea938062e7744ed6cca29c0a
avddev369 <163333594+avddev369@users.noreply.github.com>
2024-08-29 23:35:07 +0530

    Update vite.config.js
    Vite Config main configuration

--- vite.config.js
+++ vite.config.js
@@ -27,9 +27,11 @@ export default defineConfig({
     ],
   },
   server: {
-    port: 3030,
+    host:"0.0.0.0",
+    port: 8083,
   },
   preview: {
-    port: 3030,
+    host:"0.0.0.0",
+    port: 8083,
   },
 });
